109d5ffcc1409c42e9b7887af1239b6a6f5939c5
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2020-01-24 12:27:48 +0100

    a

---
+++ excod1av/ExCOD1Av.java
@@ -0,0 +1,13 @@
+
+package excod1av;
+
+
+public class ExCOD1Av {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("a");
+    }
+    
+}
